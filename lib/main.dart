a4c84e542d0f702e0672c328532a120bb6f9bf88
aguspuryanto <aguspuryanto@gmail.com>
2020-01-08 16:26:21 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 // import 'package:my_webview_project/home_page.dart';
 // import 'package:my_webview_project/MyHomePage.dart';
@@ -16,15 +19,61 @@ class MyApp extends StatelessWidget {
         ),
         // home: HomePage(),
         // home: MyHomePage(),
-        home: WebViewWebPage()
+        // home: WebViewWebPage(),
+        home: SplashScreen()
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Timer(Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => WebViewWebPage(title: 'Splash Screen Test'),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: CircleAvatar(
+            radius: 30.0,
+            backgroundColor: Colors.amberAccent,
+            foregroundColor: Colors.red,
+            child: Icon(
+              Icons.adb,
+              size: 50.0,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
 
 class WebViewWebPage extends StatefulWidget {
- @override
- _WebViewWebPageState createState() => _WebViewWebPageState();
+  WebViewWebPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _WebViewWebPageState createState() => _WebViewWebPageState();
 }
+
 class _WebViewWebPageState extends State<WebViewWebPage> {
  // URL to load
  var URL = "https://surabayajobfair.com/";
@@ -32,8 +81,30 @@ class _WebViewWebPageState extends State<WebViewWebPage> {
  // Webview progress
  double progress = 0;
  
-@override
- Widget build(BuildContext context) {
+ Future<bool> _onWillPop() {
+    return showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to exit an App'),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: Text('No'),
+              ),
+              FlatButton(
+                onPressed: () => exit(0),
+                /*Navigator.of(context).pop(true)*/
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        ) ??
+        false;
+  }
+  
+  @override
+  Widget build(BuildContext context) {
    return Scaffold(
        appBar: AppBar(
          title: Text("Webview App"),
@@ -67,5 +138,5 @@ class _WebViewWebPageState extends State<WebViewWebPage> {
            ),
          )
        ].where((Object o) => o != null).toList())));  //Remove null widgets
- }
+  }
 }
